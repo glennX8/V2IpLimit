15dc5f9ab635a184b6c240e8138c85dc2acdf64a
glennX8 <159617745+glennX8@users.noreply.github.com>
2025-06-02 20:07:05 +0500

    Update read_config.py

--- utils/read_config.py
+++ utils/read_config.py
@@ -7,23 +7,25 @@ import json
 import os
 import sys
 import time
+import logging
 
 CONFIG_DATA = None
 LAST_READ_TIME = 0
 
+logger = logging.getLogger(__name__)
 
 async def read_config(
     check_required_elements=None,
 ) -> dict:
     """
-    read and return data from a JSON file.
+    Read and return data from a JSON file.
     """
     global CONFIG_DATA
     global LAST_READ_TIME
     config_file = "config.json"
 
     if not os.path.exists(config_file):
-        print("Config file not found.")
+        logger.error("Config file not found.")
         sys.exit()
     file_mod_time = os.path.getmtime(config_file)
     if CONFIG_DATA is None or file_mod_time > LAST_READ_TIME:
@@ -31,15 +33,13 @@ async def read_config(
             with open(config_file, "r", encoding="utf-8") as f:
                 CONFIG_DATA = json.load(f)
         except json.JSONDecodeError as error:
-            print(
-                "Error decoding the config.json file. Please check its syntax.", error
-            )
+            logger.error("Error decoding the config.json file. Please check its syntax. %s", error)
             sys.exit()
         if "BOT_TOKEN" not in CONFIG_DATA:
-            print("BOT_TOKEN is not set in the config.json file.")
+            logger.error("BOT_TOKEN is not set in the config.json file.")
             sys.exit()
         if "ADMINS" not in CONFIG_DATA:
-            print("ADMINS is not set in the config.json file.")
+            logger.error("ADMINS is not set in the config.json file.")
             sys.exit()
         LAST_READ_TIME = time.time()
     if check_required_elements:
